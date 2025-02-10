decda69ae10ee729af74078f2e259f32d024bf2e
MagicIshSeal <maxvaneikeren@gmail.com>
2025-02-10 13:21:30 +0100

    Added verification code

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -10,8 +10,42 @@ void setup()
 {
   Serial.begin(115200);
   Wire.begin();
-  altitude.begin();
+
+  Serial.println(F("Altitude Sensor Test"));
+  if (!altitude.begin())
+  {
+    while (1)
+    {
+      Serial.println(F("Could not find a valid BMP280 sensor, check wiring or try a different address!"));
+      delay(10);
+    }
+  }
+
+  Serial.println(F("Accelerometer Test"));
+  if (!accel.setup(0x68))
+  {
+    while (1)
+    {
+      Serial.println(F("Could not find a valid MPU9250 sensor, check wiring or try a different address!"));
+      delay(10);
+    }
+  }
+
   delay(1000);
+  // Code to calibrate the sensors
+  /*Serial.println("Accel Gyro calibration will start in 5sec.");
+  Serial.println("Please leave the device still on the flat plane.");
+  mpu.verbose(true);
+  delay(5000);
+  mpu.calibrateAccelGyro();
+
+  Serial.println("Mag calibration will start in 5sec.");
+  Serial.println("Please Wave device in a figure eight until done.");
+  delay(5000);
+  mpu.calibrateMag();
+
+  print_calibration();
+  mpu.verbose(false);*/
 }
 
 void loop()
@@ -19,12 +53,12 @@ void loop()
   printAccelerometer();
   if (altitude.takeForcedMeasurement())
   {
-    Serial.println("Pressure: ");
-    Serial.println(altitude.readPressure());
-    Serial.print('Pa');
-    Serial.println("Temperature: ");
-    Serial.println(altitude.readTemperature());
-    Serial.print('C');
+    Serial.print("Pressure: ");
+    Serial.print(altitude.readPressure());
+    Serial.println(" Pa");
+    Serial.print("Temperature: ");
+    Serial.print(altitude.readTemperature());
+    Serial.println(" C");
   }
   else
   {
@@ -35,16 +69,16 @@ void loop()
 
 void printAccelerometer()
 {
-  Serial.print("  Yaw: ");
+  Serial.print("Yaw: ");
   Serial.print(accel.getYaw(), 6);
-  Serial.println(" Roll: ");
+  Serial.print(" Roll: ");
   Serial.print(accel.getRoll(), 6);
-  Serial.println(" Pitch: ");
+  Serial.print(" Pitch: ");
   Serial.println(accel.getPitch(), 6);
-  Serial.println("X Accel: ");
+  Serial.print("X Accel: ");
   Serial.println(accel.getAccX(), 6);
-  Serial.println("Y Accel: ");
+  Serial.print("Y Accel: ");
   Serial.println(accel.getAccY(), 6);
-  Serial.println("Z Accel: ");
+  Serial.print("Z Accel: ");
   Serial.println(accel.getAccZ(), 6);
 }
